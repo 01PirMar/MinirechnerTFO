e531940f7c29952792f77f64d4c671beede65250
01PirMar <35956764+01PirMar@users.noreply.github.com>
2020-04-23 10:51:39 +0200

    Titel

---
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinirechnerTFO
+{
+    class Program
+
+    {
+
+        // MINIRECHNERTFO
+
+        public static void Main(string[] args)
+        {
+            
+        }
+     
+    }
+}
