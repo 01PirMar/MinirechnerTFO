d50570339f8e6bd9921dfbfdb78a95d909e3964d
01PirMar <35956764+01PirMar@users.noreply.github.com>
2020-04-23 11:03:17 +0200

    Addieren

--- MinirechnerTFO/MinirechnerTFO/Program.cs
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -13,7 +13,19 @@ namespace MinirechnerTFO
 
         public static void Main(string[] args)
         {
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Addiere(Zahl1,Zahl2));
+
+            Console.ReadKey();
             
+        }
+        static int Addiere(int Zahl1, int Zahl2)
+        {
+            int erg = Zahl1 + Zahl2;
+            return (erg);
         }
      
     }
