ef49b4f6fc3684ece2643f49257cdcf466abdea2
01PirMar <35956764+01PirMar@users.noreply.github.com>
2020-04-23 11:16:32 +0200

    MultipöizierenFertig

--- MinirechnerTFO/MinirechnerTFO/Program.cs
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -13,8 +13,33 @@ namespace MinirechnerTFO
 
         public static void Main(string[] args)
         {
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Addiere(Zahl1,Zahl2));
+            Console.WriteLine(Subtrahiere(Zahl1, Zahl2));
+            Console.WriteLine(Multiplizieren(Zahl1, Zahl2));
+
+            Console.ReadKey();
             
         }
+        static int Addiere(int Zahl1, int Zahl2)
+        {
+            int erg = Zahl1 + Zahl2;
+            return (erg);
+        }
+        static int Subtrahiere(int Zahl1, int Zahl2)
+        {
+            int erg = Zahl1 - Zahl2;
+            return (erg);
+        }
+        static int Multiplizieren(int Zahl1, int Zahl2)
+        {
+            int erg = Zahl1 * Zahl2;
+            return (erg);
+        }
+
      
     }
 }
